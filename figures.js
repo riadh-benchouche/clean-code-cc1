25ec6842ddb1c5dcab0eb7c6ea123eaec87eff88
riadh-fstck <riadh@fstck.co>
2024-02-29 12:53:20 +0100

    init the functions and test

---
+++ figures.js
@@ -0,0 +1,34 @@
+const isBrelan = rolls => {
+
+};
+
+const isCarre = rolls => {
+
+};
+
+const isFull = rolls => {
+
+};
+
+const isGrandeSuite = rolls => {
+
+};
+
+
+const isYams = rolls => {
+
+}
+
+const analyzeRolls = rolls => {
+
+};
+
+module.exports = {analyzeRolls, isYams, isCarre, isFull, isGrandeSuite, isBrelan};
+
+
+
+
+
+
+
+
---
+++ test/figures.test.js
@@ -0,0 +1,25 @@
+const {analyzeRolls, isYams, isCarre, isFull, isGrandeSuite, isBrelan} = require('../figures');
+
+describe('isYams', () => {
+
+});
+
+describe('isCarre', () => {
+
+});
+
+describe('isFull', () => {
+
+});
+
+describe('isGrandeSuite', () => {
+
+});
+
+describe('isBrelan', () => {
+
+});
+
+describe('analyzeRolls', () => {
+
+});
